2756bb1ac2d77cd99a00078e52c8647ff055bf7f
Fabian Choque <fabianchoque28@gmail.com>
2022-08-26 18:48:59 -0300

    seccion registro: envio de datos del formulario

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-//
+
+// Función para realizar la petición post enviando los datos del formulario
+function onClick (event) {
+    event.preventDefault();
+    
+    const formulario = {
+        comercio: document.getElementById('comercio').value,
+        titular: document.getElementById('titular').value,
+        email: document.getElementById('email').value,
+        celular: document.getElementById('celular').value
+    }
+    console.log(formulario);
+  
+  
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify(formulario),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+    })
+        .then((response) => response.json())
+        .then((json) => { 
+            console.log(json);
+            Swal.fire(
+                'Enviado',
+                'Gracias por inscribite al evento', 
+                'success'
+            );
+            cleanForm();
+    })
+    .catch((err) => console.log(err));
+    
+}
+
+function cleanForm() {
+    let formulario = document.getElementById('formulario');    
+    formulario.reset();    
+}
+	
+let boton = document.getElementById("enviar");
+boton.addEventListener("click", onClick);
